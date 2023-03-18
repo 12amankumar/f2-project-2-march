3894a4e610b6be3297ab10f806594247eb7fa645
12amankumar <117712947+12amankumar@users.noreply.github.com>
2023-03-18 21:10:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+function OpeningCeremony() {
+  console.log("Let the games begin");
+  setTimeout(function() {
+    let score = {
+      red: 0,
+      yellow: 0,
+      green: 0,
+      blue: 0
+    };
+    Race100M(score, function(score) {
+      LongJump(score, function(score) {
+        HighJump(score, function(score) {
+          AwardCeremony(score);
+        });
+      });
+    });
+  }, 1000);
+}
+
+function Race100M(score, callbackFnc) {
+  console.log("Score before Race100M:", score);
+  setTimeout(function() {
+    let timeRed = Math.floor(Math.random() * 6) + 10;
+    let timeYellow = Math.floor(Math.random() * 6) + 10;
+    let timeGreen = Math.floor(Math.random() * 6) + 10;
+    let timeBlue = Math.floor(Math.random() * 6) + 10;
+
+    let results = [
+      { color: "red", time: timeRed },
+      { color: "yellow", time: timeYellow },
+      { color: "green", time: timeGreen },
+      { color: "blue", time: timeBlue }
+    ];
+
+    results.sort(function(a, b) {
+      return a.time - b.time;
+    });
+
+    score[results[0].color] += 50;
+    score[results[1].color] += 25;
+
+    console.log(
+      `${results[0].color} won Race100M with ${results[0].time} seconds.`
+    );
+    console.log(
+      `${results[1].color} came second in Race100M with ${results[1].time} seconds.`
+    );
+    console.log(`Score after Race100M:`, score);
+    callbackFnc(score);
+  }, 3000);
+}
+
+function LongJump(score, callbackFnc) {
+  
+}
+
+function HighJump(score, callbackFnc) {
+  
+  callbackFnc(score);
+}
+
+function AwardCeremony(score) {
+ 
+}
+
+OpeningCeremony();
